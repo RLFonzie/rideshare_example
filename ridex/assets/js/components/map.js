14fc1271f7364638ca84abdbd9b423416661cc78
alfons caroles <alfons.caroles@rocketlab.com.au>
2022-10-04 17:49:44 +1100

    Added channel to map

--- ridex/assets/js/components/map.js
+++ ridex/assets/js/components/map.js
@@ -1,17 +1,49 @@
 import React, { useState, useEffect } from "react";
-import { Map, Marker, TileLayer } from "react-leaflet";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import { Socket } from "phoenix";
 import { usePosition } from "../util/usePosition";
+import Geohash from "latlon-geohash";
+
+const geohashFromPosition = (position) =>
+  position ? Geohash.encode(position.lat, position.lng, 5) : "";
 
 export default ({ user }) => {
   const position = usePosition();
+  const [channel, setChannel] = useState();
+
+  useEffect(() => {
+    const socket = new Socket("/socket", { params: { token: user.token } });
+    socket.connect();
+
+    if (!position) {
+      return;
+    }
+
+    const phxChannel = socket.channel("cell:" + geohashFromPosition(position));
+    phxChannel.join().receive("ok", (response) => {
+      console.log("Joined channel!");
+      setChannel(phxChannel);
+    });
+
+    return () => phxChannel.leave();
+  }, [geohashFromPosition(position)]);
 
   if (!position) {
     return <div>Awaiting for position...</div>;
   }
 
+  if (!channel) {
+    return <div>Connecting to channel...</div>;
+  }
+
   return (
     <div>
       Logged in as {user.type}
+      {user.type == "rider" && (
+        <div>
+          <button onClick={requestRide}>Request Ride</button>
+        </div>
+      )}
       <Map center={position} zoom={15}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
